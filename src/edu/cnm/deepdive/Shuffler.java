2b7c0b7a2d27e49ff2169f8ab3291de1dcbd1ad7
Ricky Garcia <rickyg08@msn.com>
2020-09-29 15:44:25 -0600

    Added overloads for all primitives.

--- src/edu/cnm/deepdive/Shuffler.java
+++ src/edu/cnm/deepdive/Shuffler.java
@@ -8,16 +8,96 @@ public class Shuffler {
 
   public Shuffler(Random rng) {
     this.rng = rng;
-
   }
 
   public void shuffle(int[] data) {
     for (int i = data.length - 1; i > 0; i--)  {
       int j = rng.nextInt(i + 1);
-      int temp = data[i];
-      data[i] = data[j];
-      data[j] = temp;
+      if (i != j) {
+        var temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+      }
     }
   }
+
+  public void shuffle(long[] data) {
+    for (int i = data.length - 1; i > 0; i--)  {
+      int j = rng.nextInt(i + 1);
+      if (i != j) {
+        var temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+      }
+    }
+  }
+
+  public void shuffle(byte[] data) {
+    for (int i = data.length - 1; i > 0; i--)  {
+      int j = rng.nextInt(i + 1);
+      if (i != j) {
+        var temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+      }
+    }
+  }
+
+  public void shuffle(boolean[] data) {
+    for (int i = data.length - 1; i > 0; i--)  {
+      int j = rng.nextInt(i + 1);
+      if (i != j) {
+        var temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+      }
+    }
+  }
+
+  public void shuffle(char[] data) {
+    for (int i = data.length - 1; i > 0; i--)  {
+      int j = rng.nextInt(i + 1);
+      if (i != j) {
+        var temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+      }
+    }
+  }
+
+  public void shuffle(short[] data) {
+    for (int i = data.length - 1; i > 0; i--)  {
+      int j = rng.nextInt(i + 1);
+      if (i != j) {
+        var temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+      }
+    }
+  }
+
+  public void shuffle(float[] data) {
+    for (int i = data.length - 1; i > 0; i--)  {
+      int j = rng.nextInt(i + 1);
+      if (i != j) {
+        var temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+      }
+    }
+  }
+
+  public void shuffle(double[] data) {
+    for (int i = data.length - 1; i > 0; i--)  {
+      int j = rng.nextInt(i + 1);
+      if (i != j) {
+        var temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+      }
+    }
+  }
+
+
 
 }
